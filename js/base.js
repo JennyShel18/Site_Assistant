e741abbae2435177d94f832eb98358a0eb4ddb64
Евгения Щелкунова <EvgeniaShelkunova00@gmail.com>
2021-05-29 16:35:11 +0300

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myLinks2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
