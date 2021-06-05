3eb13f4ac20fe70418a08e47e4b8612eb56852a2
JennyShel18 <evgeniashelkunova00@gmail.com>
2021-06-05 06:34:41 +0400

    удаление статик

--- js/base.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myLinks2");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
